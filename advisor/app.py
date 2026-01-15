389f74b7e33497f8a43f58408e09a7f7174e88ab
HongXunyang <519109830@qq.com>
2026-01-15 09:33:00 +0100

    v0.2.8

--- advisor/app.py
+++ advisor/app.py
@@ -3,9 +3,10 @@
 import os
 import sys
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtCore import Qt, QLocale
+from PyQt5.QtCore import Qt, QLocale, QFile, QIODevice
 
 from advisor.controllers import AppController
+from advisor.resources import resources_rc
 
 
 def load_stylesheet() -> str:
